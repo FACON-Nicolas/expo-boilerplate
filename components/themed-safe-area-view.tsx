86cdcf1c2cc9dbb7d8a509fe65672a3ed789b8eb
nicolas_facon <faconicolas@gmail.com>
2025-07-19 18:36:20 +0200

    feat: add onboarding identity screen and integrate Lottie animation for enhanced user experience

--- components/themed-safe-area-view.tsx
+++ components/themed-safe-area-view.tsx
@@ -3,7 +3,13 @@ import {
   SafeAreaView,
   SafeAreaViewProps,
 } from 'react-native-safe-area-context';
-import { StyleSheet } from 'react-native';
+import { Dimensions, Platform, StyleSheet } from 'react-native';
+
+const WINDOW_HEIGHT = Dimensions.get('window').height;
+const WINDOW_WIDTH = Dimensions.get('window').width;
+const RATIO = Math.round((WINDOW_HEIGHT / WINDOW_WIDTH) * 100) / 100;
+const IPHONE_SE_RATIO = Math.round((568 / 320) * 100) / 100;
+
 export default function ThemedSafeAreaView({
   children,
   style,
@@ -23,5 +29,9 @@ export default function ThemedSafeAreaView({
 const styles = StyleSheet.create({
   safeAreaView: {
     flex: 1,
+    paddingBottom:
+      Platform.OS === 'ios' && RATIO !== IPHONE_SE_RATIO
+        ? 0
+        : WINDOW_HEIGHT * 0.02,
   },
 });
