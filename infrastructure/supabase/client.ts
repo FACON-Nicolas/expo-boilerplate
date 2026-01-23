bb6b6319a9d696a1aab5e4af6af9b64f30dc5dca
nicolas_facon <faconicolas@gmail.com>
2026-01-23 09:47:39 +0100

    refactor(supabase): rename env variable to SUPABASE_PUBLISHABLE_KEY

--- infrastructure/supabase/client.ts
+++ infrastructure/supabase/client.ts
@@ -1,12 +1,12 @@
-import { createClient } from '@supabase/supabase-js';
+import { createClient } from "@supabase/supabase-js";
 
 export const supabaseClient = createClient(
   process.env.EXPO_PUBLIC_SUPABASE_URL!,
-  process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY!,
+  process.env.EXPO_PUBLIC_SUPABASE_PUBLISHABLE_KEY!,
   {
     auth: {
       autoRefreshToken: false,
       persistSession: false,
     },
-  }
+  },
 );
